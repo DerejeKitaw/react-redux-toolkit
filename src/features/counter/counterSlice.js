82f76701c97b4e5c04597050d01fce4bfa785891
DerejeKitaw <dereje.kitaw@gmail.com>
2022-02-10 17:29:05 -0500

    counter sample reduc toolkit v7

--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const slice = createSlice({
+export const counterSlice = createSlice({
   name: 'counter',
   initialState: {
     value: 0,
@@ -18,12 +18,12 @@ export const slice = createSlice({
   },
 });
 
-export const { increment, decrement, incrementByAmount } = slice.actions;
-export const incrementAsync = amount => dispatch => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+// export const incrementAsync = amount => dispatch => {
+//   setTimeout(() => {
+//     dispatch(incrementByAmount(amount));
+//   }, 1000);
+// };
 export const selectCount = state => state.counter.value;
 
-export default slice.reducer;
+export default counterSlice;
